a2e89f3189bd1d596796f3d99519798094403142
Arturo Lopez <alopez@delegata.com>
2021-05-16 16:20:11 -0700

    Components

---
+++ src/CommentDetail.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const CommentDetail = props => {
+	return (
+		<div className="comment">
+			<a href="/" className="avatar">
+				<img alt="avatar" src={ props.profileImage } />
+			</a>
+			<div className="content">
+				<a href="/" className="author">
+					{ props.author }
+				</a>
+			
+				<div className="metadata">
+					<span className="date">Today at { props.postedTime }</span>
+				</div>
+				<div className="text">{ props.text }</div>
+			</div>
+		</div>
+	);
+};
+
+export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import faker from 'faker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () =>  {
+	return (
+		<div className="ui container comments">
+			<ApprovalCard>
+				<CommentDetail 
+					author="Arturo" 
+					postedTime="Today at 4:45PM" 
+					text="Hello there!" 
+					profileImage={faker.image.avatar()}
+				/>
+			</ApprovalCard>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+			<ApprovalCard>
+				<CommentDetail	
+					author="Paola" 
+					postedTime="Today at 5:45PM" 
+					text="Wow it's late!" 
+					profileImage={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+
+			<ApprovalCard>
+				<CommentDetail
+					author="Juan"
+					postedTime="Today at 6:45PM"
+					text="Never too late to say hi!!"
+					profileImage={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+		</div>
+
+	);
+};
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
